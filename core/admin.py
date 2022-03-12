b75ff02dc809955fbf7d077d487578ce0049c5ff
Lucas Quevedo <lucas.qvdo@gmail.com>
2022-03-12 17:21:35 -0300

    Criada tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display=('titulo','data_evento','data_criacao', 'usuario',)#definir o que eu quero que apareça ao lado de cada item na tabela
+    list_filter = ( 'data_evento', 'usuario',)
+
 
 # Register your models here.
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Evento(models.Model):#crio uma classe especificando como eu quero minha tabela
+    titulo=models.CharField(max_length=100)
+    descricao=models.TextField(blank=True, null=True)
+    data_evento=models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao=models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        db_table='evento'
+
+    def __str__(self):#quero que apareça o título do evento na listagem
+        return self.titulo
